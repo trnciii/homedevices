a57efa836ed3e5ae92946fec2a9266a904a5a0ac
trnciii <doityan223@gmail.com>
2021-07-08 14:25:02 +0900

    add setup

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="homedevices",
+    version="0.0",
+    packages=find_packages(),
+    url="https://github.com/trnciii/homedevices",
+    license="MIT"
+)
